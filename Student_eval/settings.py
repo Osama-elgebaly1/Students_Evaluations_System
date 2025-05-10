508a35b81377a5b1a3342fe1931fd3094d395daa
Osama Elgebaly <osamaelgebaly78@gmail.com>
2025-05-10 11:23:21 +0300

    Making it ready for production

--- Student_eval/settings.py
+++ Student_eval/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,6 +125,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static/')
 ]
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+MEDIA_ROOT = BASE_DIR / "media"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
